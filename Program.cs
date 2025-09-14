9784eb04e4ab0bc5516c46c373b71e8f418921eb
dariakarnauh <dasakarnauh2@gmail.com>
2025-09-14 14:35:35 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿class task1
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter the radius of a circle:");
+        string radiusInput = Console.ReadLine();
+        bool doubleParseSuccess = double.TryParse(radiusInput, out double radius);
+        radius = double.Parse(radiusInput);
+        double area = Math.Pow(radius, 2);
+        Console.WriteLine($"The area of a circle with radius {radiusInput} is {area}pi");
+    }
+}
+//Done
+
