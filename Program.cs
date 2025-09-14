3c7c110f5450615221b57718713ecfe0425a3909
dariakarnauh <dasakarnauh2@gmail.com>
2025-09-14 15:53:05 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿class Task2
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter a Tempterature in Celsius: ");
+        string input = Console.ReadLine();
+        bool isSuccess = int.TryParse(input, out int numberInput);
+        if (numberInput > 0)
+        {
+            Console.WriteLine("It's above freezing!");
+        }
+        else if (numberInput < 0)
+        {
+            Console.WriteLine("Freezing");
+        }
+        else
+        {
+            Console.WriteLine("Invalid input");
+        }
+    }
+}
+//Done.
